99a59ac79168afd0ea24d4bf3d51d19b47571acd
Fabiano Araujo <fabiano.fa04@gmail.com>
2014-07-16 14:26:44 -0300

    CV v2

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,18 @@
+var callback = function(){
+	$('.item-skills').each(function(){
+		newWidth = $(this).parent().width() * $(this).data('percent');
+		$(this).width(0);
+    $(this).animate({
+        width: newWidth,
+    }, 1000);
+	});
+};
+$(document).ready(callback);
+
+var resize;
+window.onresize = function() {
+	clearTimeout(resize);
+	resize = setTimeout(function(){
+		callback();
+	}, 100);
+};
