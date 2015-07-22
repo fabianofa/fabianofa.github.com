89971ff6132f562e3d2592d13edc0e4a1e60ce94
Fabiano <fabiano.fa04@gmail.com>
2015-07-22 20:57:33 -0300

    Remoção do uso de isotope

--- js/custom.js
+++ js/custom.js
@@ -7,13 +7,13 @@ var customScripts = {
             var $container = $('.isotopeWrapper');
             var $resize = $('.isotopeWrapper').attr('id');
             // initialize isotope
-            $container.isotope({
-                itemSelector: '.isotopeItem',
-                resizable: false, // disable normal resizing
-                masonry: {
-                    columnWidth: $container.width() / $resize
-                }
-            });
+            // $container.isotope({
+            //     itemSelector: '.isotopeItem',
+            //     resizable: false, // disable normal resizing
+            //     masonry: {
+            //         columnWidth: $container.width() / $resize
+            //     }
+            // });
             $("a[href='#top']").click(function () {
                 $("html, body").animate({ scrollTop: 0 }, "slow");
                 return false;
@@ -21,23 +21,23 @@ var customScripts = {
             $('.navbar-inverse').on('click', 'li a', function () {
                 $('.navbar-inverse .in').addClass('collapse').removeClass('in').css('height', '1px');
             });
-            $('#filter a').click(function () {
-                $('#filter a').removeClass('current');
-                $(this).addClass('current');
-                var selector = $(this).attr('data-filter');
-                $container.isotope({
-                    filter: selector
-                });
-                return false;
-            });
-            $(window).smartresize(function () {
-                $container.isotope({
-                    // update columnWidth to a percentage of container width
-                    masonry: {
-                        columnWidth: $container.width() / $resize
-                    }
-                });
-            });
+            // $('#filter a').click(function () {
+            //     $('#filter a').removeClass('current');
+            //     $(this).addClass('current');
+            //     var selector = $(this).attr('data-filter');
+            //     $container.isotope({
+            //         filter: selector
+            //     });
+            //     return false;
+            // });
+            // $(window).smartresize(function () {
+            //     $container.isotope({
+            //         // update columnWidth to a percentage of container width
+            //         masonry: {
+            //             columnWidth: $container.width() / $resize
+            //         }
+            //     });
+            // });
         }
     },
     fancybox: function () {
